58266f9c95ee51f68db40f872e06e4556dc8fe60
andrz <andr.zahar2@gmai.com>
2023-06-04 03:04:47 +0500

    Initial Commit

---
+++ spline/splineApp.swift
@@ -0,0 +1,17 @@
+//
+//  splineApp.swift
+//  spline
+//
+//  Created by Андрей Захаров on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct splineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
